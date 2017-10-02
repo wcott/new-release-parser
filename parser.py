44cbcbcc6b2165f4dc823bb5a33f640ab91b0558
Scott Walter <swalter@cray.com>
2017-10-02 14:41:08 -0500

    categorizing teasers but not new releases, and crowd funding apart from the rest

--- parser.py
+++ parser.py
@@ -23,7 +23,7 @@ with open('release.tsv') as f:
 for i in range(0, len(name_list) - 1):
     # if there is more than one release per mfg, I want to only have the mfg
     # listed once. So, I'm modeling my data structure after this idea.
-    
+
     if "Crowd Funding" in category_list[i]:
         crowd_funding_formatted_list.append("* [{0}]({1})\n".format(mfg_list[i], link_list[i]))
     else:
@@ -41,24 +41,30 @@ for i in range(0, len(name_list) - 1):
 date = date.today().isoformat()
 with open('archive/{0}'.format(date), "w") as f:
     f.write("What up mini people?! Got some more new releases, teasers, and kickstarters for ya!\n\n\n")
-    f.write("*New Releases and Teasers*\n\n\n")
+    f.write("**New Releases and Teasers**\n\n\n")
     for mfg, values in new_release_dict.iteritems():
         if len(values) == 1:
             release_dict = values[0]
-            f.write("[{0}: {1} - {2}]({3})\n\n\n".format(mfg, release_dict['name'], release_dict['category'], release_dict['link']))
+            if "Teaser" in release_dict['category']:
+                f.write("[{0}: {1} - {2}]({3})\n\n\n".format(mfg, release_dict['name'], release_dict['category'], release_dict['link']))
+            else:
+                f.write("[{0}: {1}]({2})\n\n\n".format(mfg, release_dict['name'], release_dict['link']))
         else:
             f.write("{0}\n".format(mfg))
             f.write("\n")
             for value in values:
-                f.write("* [{0} - {1}]({2})\n".format(value['name'], value['category'], value['link']))
+                if "Teaser" in value['category']:
+                    f.write("* [{0} - {1}]({2})\n".format(value['name'], value['category'], value['link']))
+                else:
+                    f.write("* [{0}]({1})\n".format(value['name'], value['link']))
             f.write("\n\n")
-    f.write("\nCrowd Funding Campaigns\n\n")
+    f.write("\n**Crowd Funding Campaigns**\n\n")
     for item in crowd_funding_formatted_list:
         f.write(item)
 
     f.write("\n\n\n")
     f.write("If I missed any releases in the last two weeks don't hesitate to "
-            "list them and I will update the post.If you see any mistakes/broken " 
+            "list them and I will update the post.If you see any mistakes/broken "
             "links or have any questions, feel free to message me or comment on the thread. PAINT MORE MINIS!\n\n")
     f.write("[archive of older posts](https://www.reddit.com/r/minipainting/wiki/newreleasesarchive)\n\n")
     f.write("Would you like to help find the new releases that go into this post? Private message /u/SDuby!")
